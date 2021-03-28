650d2710cfc2e4d1f3c2d3d20e2913881e7b8a5d
Huntthee <mikeallison000@gmail.com>
2021-03-27 21:53:30 -0600

    all the wonderful workings!

--- disc-bot.js
+++ disc-bot.js
@@ -38,6 +38,40 @@ const norrisJoke = [
   }
 ]
 
+//A bunch of random quotes by your loyal destructor, Nic (a.k.a. FlameScion)
+const quoteRando = [
+  {
+    quote: "I don't wanna be horny anymore, I just wanna be happy...."
+  },
+  {
+    quote: "Damn you humans! Get off my virtual yard."
+  },
+  {
+    quote: "Cthuwu is a lewd deity, living in the bottom of Hornysmouth."
+  },
+  {
+    quote: "Remember to stay hydrated. Water may not need you, but you need water."
+  },
+  {
+    quote: "A nice, crispy churro is as close as you'll get to heaven in life."
+  },
+  {
+    quote: "FlameScion rules, and I am not biased at all whatsoever."
+  },
+  {
+    quote: "Hello there."
+  },
+  {
+    quote: "NEEEEEEEEEEEERRRRRD."
+  },
+  {
+    quote: "My code was nice, clean and well organized. But then a certain argentine douche came and made a mess"
+  },
+  {
+    quote: " 'I spent so much time thinking if I could, I never stopped to think if I should'-FlameScion, trying to get this FUCKING COMMAND working "
+  }
+]
+
 
 // When the bot connects to the server.
 client.on('ready', () => {
@@ -58,7 +92,8 @@ client.on('ready', () => {
   let generalChannel = client.channels.cache.get("782832103913029672")
   const attachment = new Discord.MessageAttachment("assets/truth.png")
   // generalChannel.send(attachment)
-  // generalChannel.send("I'm in here now.")
+  generalChannel.send("Im a real boy!")
+
 })
 
 /* This block will can return messages/images/emotes anytime a message is detected in channel.
@@ -119,6 +154,9 @@ function processCommand(receivedMessage) {
   } else if (primaryCommand == "roll") {
     rollCommand(arguments, receivedMessage)
 
+  } else if (primaryCommand == "quote") {
+    quoteCommand(arguments, receivedMessage)
+
     // ADD MORE COMMANDS HERE with more else if 
 
   } else {
@@ -145,7 +183,7 @@ function helpCommand(arguments, receivedMessage) {
     receivedMessage.channel.send("Hey there! Try `!help commands` to see what you can do.")
   
   } else if (arguments == "commands"){
-    receivedMessage.channel.send("The commands you can use so far are `!mult, !add, !size, !roll (to roll a D20) and !joke`")
+    receivedMessage.channel.send("The commands you can use so far are `!mult, !add, !size, !roll (to roll a D20), !joke for some terrible Chuck Norris jokes and !quote for some wonderful quotes supplied by FlameScion!`")
 
   } else {
     receivedMessage.channel.send("It looks like you need help with " + arguments + " but I'm too dumb right now.")
@@ -162,7 +200,7 @@ function jokeCommand(arguments, receivedMessage) {
   receivedMessage.channel.send(`${norrisJoke[Math.floor(Math.random() * norrisJoke.length)].joke}`)
 }
 
-// The adding command!
+// The Adding command!
 function addCommand(arguments, receivedMessage) {
   if (arguments.length < 2) {
     receivedMessage.channel.send("Not enough maths! Try `!add 2 2`")
@@ -175,20 +213,25 @@ function addCommand(arguments, receivedMessage) {
   receivedMessage.channel.send(arguments + " = " + product.toString())
 }
 
-// Roll the Die!
+// Roll the Die! Refactored from my Twitch D20 bot.
 function rollCommand(arguments, receivedMessage) { 
   const sides = 20;
   const roll = Math.floor(Math.random() * sides) + 1;
 
   if (roll == 20) {
-    receivedMessage.channel.send(`WHOA! Nice Crits, You rolled a ${roll}`)
+    receivedMessage.channel.send(`DAMN SON!, You rolled a ${roll}!`)
   } else if (roll == 1) {
-    receivedMessage.channel.send(`OOF, That's gotta hurt. You rolled a ${roll}`)
-  } else {
+    receivedMessage.channel.send(`HA!, Sucker. You just rolled a ${roll}`)
+  } else if (roll== 5){
+    receivedMessage.channel.send(`You just rolled a ${roll}. Doc disliked that....`)
+  }else {
     receivedMessage.channel.send(`You rolled a ${roll}`)
   }
 }
 
+// Flames' extremely volatile quote
+function quoteCommand(arguments, recievedMessage){
+  recievedMessage.channel.send(`${quoteRando[Math.floor(Math.random() * quoteRando.length)].quote}`)
+}
 
-
-client.login("")
+client.login("ODIyNjY0ODczMzQ3MTIxMTYy.YFVkXg.zYzR3hwzHFYz1cVj6wNKcLi-cM4")
