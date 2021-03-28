54c4820aa94ad289b0a2aae276d2521b3f2fb959
Huntthee <mikeallison000@gmail.com>
2021-03-28 09:55:49 -0600

    added music player functionality!

--- disc-bot.js
+++ disc-bot.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js')
+const ytdl = require("ytdl-core");
+const prefix = "?";
+
 const client = new Discord.Client()
+
+const queue = new Map();
 
 // List of random nouns hehe
 const noun = [
@@ -155,7 +160,7 @@ client.on('ready', () => {
 
 })
 
-/* This block will can return messages/images/emotes anytime a message is detected in channel.
+/* This block will return messages/images/emotes anytime a message is detected in channel.
 Leave it on to detect ! commands from the users */
 client.on('message', (receivedMessage) => {
   if (receivedMessage.author == client.user) {
@@ -193,6 +198,7 @@ function processCommand(receivedMessage) {
   let primaryCommand = splitCommand[0]
   let arguments = splitCommand.slice(1)
 
+  const serverQueue = queue.get(message.guild.id);
 
   // The list of Commands grows!
   if (primaryCommand == "help") {
@@ -293,4 +299,117 @@ function quoteCommand(arguments, recievedMessage){
   recievedMessage.channel.send(`${quoteRando[Math.floor(Math.random() * quoteRando.length)].quote}`)
 }
 
-client.login("")
+// ****** The Music Player!! ****** //
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else {
+    message.channel.send("You need to enter a valid command!");
+  }
+});
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+    title: songInfo.videoDetails.title,
+    url: songInfo.videoDetails.video_url
+  };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} has been added to the queue!`);
+  }
+}
+
+function skip(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  if (!serverQueue)
+    return message.channel.send("There is no song that I could skip!");
+  serverQueue.connection.dispatcher.end();
+}
+
+function stop(message, serverQueue) {
+  if (!message.member.voice.channel)
+    return message.channel.send(
+      "You have to be in a voice channel to stop the music!"
+    );
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+  console.log(song.title)
+}
+
+// ******                    ****** //
+
+client.login("ODIyNjY0ODczMzQ3MTIxMTYy.YFVkXg.DOzK8fNvN1ctKFfS1xyfZ8Xoar4")
