eaa79f828488dddc2d5978391fa8af942eeddd01
Huntthee <mikeallison000@gmail.com>
2021-04-01 21:23:39 -0600

    added the d100

--- commands/dice.js
+++ commands/dice.js
@@ -1,5 +1,5 @@
 // Roll the Die! Refactored from my Twitch D20 bot.
-function rollCommand(arguments, receivedMessage) { 
+function roll20Command(arguments, receivedMessage) { 
   const sides = 20;
   const roll = Math.floor(Math.random() * sides) + 1;
 
@@ -14,4 +14,23 @@ function rollCommand(arguments, receivedMessage) {
   }
 }
 
-module.exports = rollCommand
+// Roll the D100!
+function roll100Command(arguments, receivedMessage) { 
+  const sides = 100;
+  const roll = Math.floor(Math.random() * sides) + 1;
+
+  if (roll == 100) {
+    receivedMessage.channel.send(`You rolled a ${roll}!. You probably cheated.`)
+  } else if (roll == 1) {
+    receivedMessage.channel.send(`Not very lucky are you?. You just rolled a ${roll}`)
+  } else if (roll== 69){
+    receivedMessage.channel.send(`You just rolled a ${roll}. Hehe, nice.`)
+  } else if (roll==42){
+    receivedMessage.channel.send(`You just rolled a ${roll}. The meaning of life is at your grasp.`)
+  } else {
+    receivedMessage.channel.send(`You rolled a ${roll}`)
+  }
+}
+
+module.exports = roll100Command
+module.exports = roll20Command
--- disc-bot.js
+++ disc-bot.js
@@ -2,15 +2,15 @@
 const Discord = require('discord.js')
 const ytdl = require("ytdl-core")
 
-// Require all the commands
+// Require all the ! commands
 const helpCommand = require('./commands/help')
 const jokeCommand = require('./commands/jokes')
 const quoteCommand = require('./commands/quotes')
 const sizeCommand = require('./commands/size')
 const multCommand = require('./commands/mult')
 const addCommand = require('./commands/add')
-const rollCommand = require('./commands/dice')
-const execute = require('./commands/execute')
+const roll20Command = require('./commands/dice')
+const roll100Command = require('./commands/dice')
 
 // Music Player listener prefix
 const prefix = "?"
@@ -100,8 +100,11 @@ function processCommand(receivedMessage) {
   } else if (primaryCommand == "add") {
     addCommand(arguments, receivedMessage)
 
-  } else if (primaryCommand == "roll") {
-    rollCommand(arguments, receivedMessage)
+  } else if (primaryCommand == "d20") {
+    roll20Command(arguments, receivedMessage)
+
+  } else if (primaryCommand == "d100"){
+    roll100Command(arguments, receivedMessage)
 
   } else if (primaryCommand == "quote") {
     quoteCommand(arguments, receivedMessage)
@@ -115,6 +118,7 @@ function processCommand(receivedMessage) {
 
 // ****** The Music Player!! ****** //
 
+// Listen for commands starting with ? as defined by prefix
 client.on("message", async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
@@ -134,6 +138,76 @@ client.on("message", async message => {
     message.channel.send("You need to enter a valid command!");
   }
 });
+
+async function execute(message, serverQueue) {
+  const args = message.content.split(" ");
+
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel)
+    return message.channel.send(
+      "You need to be in a voice channel to play music!"
+    );
+  const permissions = voiceChannel.permissionsFor(message.client.user);
+  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
+    return message.channel.send(
+      "I need the permissions to join and speak in your voice channel!"
+    );
+  }
+
+  const songInfo = await ytdl.getInfo(args[1]);
+  const song = {
+    title: songInfo.videoDetails.title,
+    url: songInfo.videoDetails.video_url
+  };
+
+  if (!serverQueue) {
+    const queueContruct = {
+      textChannel: message.channel,
+      voiceChannel: voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 5,
+      playing: true
+    };
+
+    queue.set(message.guild.id, queueContruct);
+
+    queueContruct.songs.push(song);
+
+    try {
+      var connection = await voiceChannel.join();
+      queueContruct.connection = connection;
+      play(message.guild, queueContruct.songs[0]);
+    } catch (err) {
+      console.log(err);
+      queue.delete(message.guild.id);
+      return message.channel.send(err);
+    }
+  } else {
+    serverQueue.songs.push(song);
+    return message.channel.send(`${song.title} has been added to the queue!`);
+  }
+}
+
+function play(guild, song) {
+  const serverQueue = queue.get(guild.id);
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    })
+    .on("error", error => console.error(error));
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+  console.log(song.title)
+}
 
 function skip(message, serverQueue) {
   if (!message.member.voice.channel)
